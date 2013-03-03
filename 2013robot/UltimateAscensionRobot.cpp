db12c56b1dc3f497958c014ec460f3669f5e09b9
ratpack <scottemorton@comcast.net>
2013-03-03 15:30:46 -0500

    reassigned victors

--- 2013robot/UltimateAscensionRobot.cpp
+++ 2013robot/UltimateAscensionRobot.cpp
@@ -7,24 +7,20 @@ class UltimateAscensionRobot : public IterativeRobot {
 
 	
 	//Drive train PWM channels:
-	static const int DRIVE_RIGHT_PWM = 1;
-	static const int DRIVE_LEFT_PWM = 2;
+	static const int DRIVE_RIGHT_PWM = 10;
+	static const int DRIVE_LEFT_PWM = 1;
 	
 	//Roller PWM channels:
-	static const int ELEVATOR_FRONT_PWM = 5;
-	static const int ELEVATOR_BACK_PWM = 6;
-	static const int FEEDER_PWM = 7;
-	static const int PICK_UP_PWM = 8;
+	static const int ELEVATOR_FRONT_PWM = 2;
+	static const int ELEVATOR_BACK_PWM = 3;
+	static const int FEEDER_PWM = 8;
+	static const int PICK_UP_PWM = 9;
+	static const int BUMP_UP_PWM = 7;
 	
 	//shooter PWM
-	static const int FLYWHEEL_PWM = 9;
-	static const int TIPPER_PWM = 10;
+	static const int FLYWHEEL_PWM = 5;
+	static const int TIPPER_PWM = 6;
 	
-	//These let us change the direction of the rollers (1 or -1):
-	static const int ELEVATOR_FRONT_DIRECTION = 1;
-	static const int ELEVATOR_BACK_DIRECTION = 1;
-	
-
 	//Encoder channels:
 	static const int ENCODER_DRIVE_RIGHT_A_CHANNEL = 1;
 	static const int ENCODER_DRIVE_RIGHT_B_CHANNEL = 2;
@@ -39,24 +35,11 @@ class UltimateAscensionRobot : public IterativeRobot {
 	//needs to be 1 or 2 or it WON'T WORK!
 	static const int GYRO_CHANNEL = 2;
 	
-	//These let us change the direction of the elevator (1 or -1):
+	//constants for which elevator directions are forwards
+	static const int ELEVATOR_FRONT_DIRECTION = 1;
+	static const int ELEVATOR_BACK_DIRECTION = 1;
 	static const int ELEVATOR_TOP_DIRECTION = 1;
-	static const int ELEVATOR_BOTTOM_DIRECTION = 1;
 	static const int PICKUP_DIRECTION = 1;
-	
-	//These buttons control pick-up roller speed: 
-	static const int RUN_ROLLERS_1 = 7;
-	static const int RUN_ROLLERS_2 = 8;
-
-	//Buttons used to shift into high and low gear:
-	static const int SHIFT_HIGH_BUTTON_1 = 5;
-	static const int SHIFT_HIGH_BUTTON_2 = 6;
-	static const int SHIFT_LOW_BUTTON_1 = 7;
-	static const int SHIFT_LOW_BUTTON_2 = 8;
-	
-	//shooter control button(s):
-	static const int FIRE_SHOOTER_BUTTON_1 = 5;
-	static const int FIRE_SHOOTER_BUTTON_2 = 6;
 	
 	//Solenoid channels:
 	static const int GEAR_SHIFT_SOLENOID_CHANNEL = 1;
@@ -70,6 +53,24 @@ class UltimateAscensionRobot : public IterativeRobot {
 	static const bool SHOOTER_DEPLOYED = true;
 	static const bool FEEDER_DEPLOYED = true;
 	
+	//pilot controls
+	//during disabled mode, pressing the upper shoulder buttons switches to arcade drive
+	//and the lower shoulder buttons switch to tank drive
+	//tank drive uses the left-y and right-y for left and right speed, respectively
+	//arcade drive uses the left-y and right-x for forward speed and direction, respectively
+	//in either mode, pressing up or down on the dpad will move the robot slightly forward or backwards
+	//and pressing left or right will rotate slightly in that direction w/o moving forward or back
+	static const int SHIFT_HIGH_BUTTON_1 = 5;
+	static const int SHIFT_HIGH_BUTTON_2 = 6;
+	static const int SHIFT_LOW_BUTTON_1 = 7;
+	static const int SHIFT_LOW_BUTTON_2 = 8;
+	
+	//copilot controls
+	//pressing up or down on the dpad adjusts throttle speed
+	static const int FIRE_SHOOTER_BUTTON_1 = 5;
+	static const int FIRE_SHOOTER_BUTTON_2 = 6;
+	static const int RUN_ROLLERS_1 = 7;
+	static const int RUN_ROLLERS_2 = 8;
 	
 	//Floats which are used for roller and elevator speeds:
 	float rollerSpeed;
@@ -215,6 +216,7 @@ public:
 	}
 	
 	void DisabledPeriodic(){
+		//Switches between arcade drive and tank drive
 		drive->ArcadeDrive(0.0f, 0.0f);
 		if (pilot->GetNumberedButton(5) || pilot->GetNumberedButton(6)){
 			arcade_drive = true;
@@ -233,7 +235,7 @@ public:
 		drive->ArcadeDrive(0.5f, 0.0f);
 	}
 	
-	void TeleopPeriodic(){	
+	void TeleopPeriodic(){
 		if (pilot->GetNumberedButton(SHIFT_LOW_BUTTON_1) || pilot->GetNumberedButton(SHIFT_LOW_BUTTON_2)){
 			gear_shift->Set(LOW_GEAR);
 		} else if (pilot->GetNumberedButton(SHIFT_HIGH_BUTTON_1) || pilot->GetNumberedButton(SHIFT_HIGH_BUTTON_2)){
@@ -242,7 +244,8 @@ public:
 		
 		if (gear_shift->Get() == HIGH_GEAR)
 			lcd->PrintfLine(DriverStationLCD::kUser_Line4, "in high gear");
-		//small adjustments are only allowed if we're in low gear
+		//Small adjustments are only allowed if we're in low gear
+		//Allows pilot to make small adjustments with the D-pad
 		else if (gear_shift->Get() == LOW_GEAR) {
 			lcd->PrintfLine(DriverStationLCD::kUser_Line4, "in low gear");
 			if (pilot->GetDPad() == Gamepad::kRight){
@@ -259,6 +262,8 @@ public:
 			}
 		}
 		
+		//This makes the inputs into proper arcade drive outputs if the robot is in arcade drive.
+		//Otherwise, it goes to tank drive.
 		if(arcade_drive){
 			drive->ArcadeDrive(pilot->GetLeftY(), pilot->GetRightX());
 			lcd->PrintfLine(DriverStationLCD::kUser_Line1, "In arcade drive");
@@ -267,7 +272,7 @@ public:
 			lcd->PrintfLine(DriverStationLCD::kUser_Line1, "In tank drive");
 		}
 		
-		//starts shooter when button is pressed:
+		//Starts shooter when button is pressed:
 		if (copilot->GetNumberedButton(FIRE_SHOOTER_BUTTON_1) || copilot->GetNumberedButton(FIRE_SHOOTER_BUTTON_2)){
 			shooter->flywheel->Set(throttle);
 			lcd->PrintfLine(DriverStationLCD::kUser_Line2, "Engaged at: %d%%", (int) (throttle * 100));
@@ -275,7 +280,8 @@ public:
 			shooter->flywheel->Set(0);
 			lcd->PrintfLine(DriverStationLCD::kUser_Line2, "Disengaged at: %d%%", (int) (throttle * 100));
 		}
-		
+
+		//Gives the copilot control of the shooter throttle
 		if (copilot->GetDPad() == Gamepad::kUp && throttle <= 0.95){
 			throttle += 0.05;
 		}
